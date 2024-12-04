23fcec84dbfb532d2e23fa912836f588656e2848
Raven <ravendevteam@proton.me>
2024-12-03 20:27:23 -0600

    Updated Windows build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-nuitka --onefile --standalone --windows-icon-from-ico=ICON.ico --windows-console-mode=disable --output-dir=dist toolbox.py
+nuitka --onefile --standalone --windows-icon-from-ico=ICON.ico --output-dir=dist toolbox.py
 pause
